698ff774e929539946dd5d5858cdedae4617b55e
Shriram R.S <shriramrs24@gmail.com>
2025-06-19 21:03:58 +0530

    Organized files and folders

--- Design Patterns and Principles/FactoryMethodPatternExample/PDFDocumentFactory.java
+++
@@ -1,7 +0,0 @@
-public class PDFDocumentFactory extends DocumentFactory{
-
-    public Document createDocument() {
-        return new PdfDocument();
-    }
-    
-}
--- Design Patterns and Principles/SingletonPatternExample/Application.java
+++
@@ -1,7 +0,0 @@
-public class Application {
-    public static void main(String[] args) {
-        Logger logger1 = Logger.getInstance();
-        Logger logger2 = Logger.getInstance();
-        System.out.println(logger1 == logger2);
-    }
-}
