246139eb0c98ce62362a1b4b46dfc5cef7161dc6
Shriram R.S <shriramrs24@gmail.com>
2025-06-28 11:59:29 +0530

    Exercise 1: Mocking and Stubbing

---
+++ Week2/TDDusingJUnit5andMockito/MockingandStubbing/src/main/java/MyService.java
@@ -0,0 +1,16 @@
+
+interface ExternalApi {
+    String getData();
+}
+
+public class MyService {
+    private final ExternalApi api;
+
+    public MyService(ExternalApi api) {
+        this.api = api;
+    }
+
+    public String fetchData() {
+        return api.getData();
+    }
+}
