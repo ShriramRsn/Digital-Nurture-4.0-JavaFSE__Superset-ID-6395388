6976b4b7d70fc7b9c76634f0fc8f06557ce4d081
Shriram R.S <shriramrs24@gmail.com>
2025-07-03 16:21:10 +0530

    Exercise 2: Implementing Dependency Injection

---
+++ Week3/DependencyInjection/src/main/java/DependencyInjection/BookService.java
@@ -0,0 +1,27 @@
+package DependencyInjection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookService {
+//    @Autowired
+    public BookRepository bookRepo;
+
+    public BookService(){
+        System.out.println("Book Service is created!!!");
+    }
+
+//    @Autowired
+//    public void setBookRepo(BookRepository bookRepo) {
+//        this.bookRepo = bookRepo;
+//    }
+    public void setBookRepo(BookRepository bookRepo) {
+        this.bookRepo = bookRepo;
+    }
+
+    public void bookservice(){
+        System.out.println("Books are managed");
+        bookRepo.bookrepo();
+    }
+}
