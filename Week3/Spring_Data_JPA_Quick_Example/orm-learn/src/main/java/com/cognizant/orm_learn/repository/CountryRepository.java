90fb57a898264e378560c63029d4c791dd05175e
Shriram R.S <shriramrs24@gmail.com>
2025-07-05 23:36:22 +0530

    Spring Data JPA Quick Example

---
+++ Week3/Spring_Data_JPA_Quick_Example/orm-learn/src/main/java/com/cognizant/orm_learn/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+// File: src/main/java/com/cognizant/orm_learn/repository/CountryRepository.java
+package com.cognizant.orm_learn.repository;
+
+import com.cognizant.orm_learn.model.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, String> {
+}
