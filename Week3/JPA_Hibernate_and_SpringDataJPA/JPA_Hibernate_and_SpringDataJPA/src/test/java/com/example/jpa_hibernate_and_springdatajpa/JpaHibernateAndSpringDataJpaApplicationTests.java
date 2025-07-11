7a3403f42944010c8769cd2398061cc8cb27c7c5
Shriram R.S <shriramrs24@gmail.com>
2025-07-11 13:21:30 +0530

    Create a Spring Web Project using Maven

---
+++ Week3/JPA_Hibernate_and_SpringDataJPA/JPA_Hibernate_and_SpringDataJPA/src/test/java/com/example/jpa_hibernate_and_springdatajpa/JpaHibernateAndSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpa_hibernate_and_springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateAndSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
